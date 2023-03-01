79f23c5b060e95094de7cb67a6aa34bc2d1f85c2
Alexandru Streltov <streltovalexandru19@stud.ase.ro>
2023-03-01 10:20:48 +0200

    modificari

---
+++ vhjghjghj/src/cts/s02/principii_clean_code/clase/reader/AngajatReader.java
@@ -0,0 +1,5 @@
+package cts.s02.principii_clean_code.clase.reader;
+
+public class AngajatReader implements AplicantReader {
+
+}
---
+++ vhjghjghj/src/cts/s02/principii_clean_code/clase/reader/AplicantReader.java
@@ -0,0 +1,5 @@
+package cts.s02.principii_clean_code.clase.reader;
+
+public interface AplicantReader {
+    List<Aplicant> readAplicants(String filename)
+}
